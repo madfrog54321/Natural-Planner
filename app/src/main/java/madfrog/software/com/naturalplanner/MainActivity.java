d3f0ada25720e48138d8bb14363c5679d910c4ee
madfrog54321 <konnorfrog@gmail.com>
2016-01-04 21:47:30 -0700

    Created Android Studio Project
    I generated a basic Android Studio Project. The min SDK version has been
    set to 16. This means the app will run on Android devices running
    Android Jelly Bean 4.1 and after. This may be changed in a later update.

---
+++ app/src/main/java/madfrog/software/com/naturalplanner/MainActivity.java
@@ -0,0 +1,13 @@
+package madfrog.software.com.naturalplanner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
